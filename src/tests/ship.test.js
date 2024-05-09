b05e7b100245f6eee09a1ee768aace5ff603e408
alexdilion <alexdilion@protonmail.com>
2024-05-09 11:50:44 +0100

    Feature: Add ship class

---
+++ src/tests/ship.test.js
@@ -0,0 +1,38 @@
+import Ship from "../scripts/model/Ship";
+
+test("Creates ship given a valid argument", () => {
+    const ship = new Ship(5);
+
+    expect(ship.length).toEqual(5);
+    expect(ship.hits).toEqual(0);
+});
+
+test("Hit method increases hits count", () => {
+    const ship = new Ship(5);
+    ship.hit();
+
+    expect(ship.hits).toEqual(1);
+});
+
+test("Hits does not exceed ship length", () => {
+    const ship = new Ship(2);
+
+    for (let i = 0; i < 10; i++) {
+        ship.hit();
+    }
+
+    expect(ship.hits).toEqual(2);
+});
+
+test("Ship is sunk if hits exceed ship length", () => {
+    const ship = new Ship(1);
+    ship.hit();
+
+    expect(ship.isSunk()).toBe(true);
+});
+
+test("Ship is not sunk if hits is less than ship length", () => {
+    const ship = new Ship(1);
+
+    expect(ship.isSunk()).toBe(false);
+});
