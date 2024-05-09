dfd92992f9b7e0328a34e9fb4a3c52b27de0c819
alexdilion <alexdilion@protonmail.com>
2024-05-09 15:02:46 +0100

    Feature: Add ship settings file

---
+++ src/scripts/misc/ship-settings.js
@@ -0,0 +1,17 @@
+export default {
+    Carrier: {
+        length: 5,
+    },
+    Battleship: {
+        length: 4,
+    },
+    Cruiser: {
+        length: 3,
+    },
+    Submarine: {
+        length: 3,
+    },
+    Destroyer: {
+        length: 2,
+    },
+};
